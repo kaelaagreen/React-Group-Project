d1f6703446d1f3de6aa32b60949ab02c71710a29
Matthew Boeving <mboeving1@gmail.com>
2021-09-17 17:41:47 -0400

    took out matt.tsx

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -9,12 +9,12 @@ export default function SearchResults(){
     const [recipe, setRecipe] = useState<recipe>();
 
 //all of our data is here
-    useEffect(() => {
-        //this is where the setQuery should go maybe? 
-		GetRecipe({query}:Props).then((data) => {
-			setRecipe(data);
-		});
-	}, [])
+    // useEffect(() => {
+    //     //this is where the setQuery should go maybe? 
+	// 	GetRecipe({query}:Props).then((data) => {
+	// 		setRecipe(data);
+	// 	});
+	// }, [])
 
     // const food = React.createContext("chicken")
     return (
