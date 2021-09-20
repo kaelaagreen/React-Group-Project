64c556321e2e040a3c0b1c1073a242402c8c22c6
Matthew Boeving <mboeving1@gmail.com>
2021-09-20 19:14:21 -0400

    Added Some CSS

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -4,15 +4,19 @@ import { recipeInterface } from "../models/recipe";
 
 export default function Results({label,image,source,url, calories, totalTime, recipeNumber}: recipeInterface) {
     return (
-    <div>
+    <div className="RecipeBlock">
         <Link to={"/" + recipeNumber}>
             <img src={image}/>
         </Link>
         <h3>{label}</h3>
         <p>Source: {source}</p>
         <a href={url}>Link</a>
-        <p>Calories: {calories.toFixed(0)}</p>
-        <p>Total time: {totalTime}</p>
+        {/* <p>Calories: {calories.toFixed(0)}</p>
+        <p>Total time: {totalTime}</p> */}
+        <a href="" onClick={(event) =>{
+            event?.preventDefault()
+        console.log("it clicked")
+        }}><i className="fas fa-heart"></i>Add To Favorites</a>
     </div>
     )
 }
